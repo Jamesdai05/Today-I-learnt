08678f2c315108e2800033dc7f28d760f3f12543
jamesdai05 <dai_guilong@hotmail.com>
2025-03-13 16:34:20 +0800

    fixed the connection of supabase

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,35 @@
 // import { initialFacts } from "../../data.js";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Card from "../Card/Card.jsx";
 import Sidebar from "../Sidebar/Sidebar.jsx";
 import "./main.css";
+import supabase from "../../config/supabaseClient.js";
+import { initialFacts } from "../../data.js";
+
 
 const Main = () => {
 
   const [facts,setFacts]=useState([]);
 
-  const 
+  const [fetchError,setFetchError]=useState(null);
+
+  useEffect(()=>{
+    const fetchData=async()=>{
+      const {data,error} = await supabase.from(facts).select();
+
+      if(error){
+        setFetchError("Unable to get the data");
+        console.log(error);
+        setFacts(null);
+      };
+      if(facts){
+        console.log(facts)
+        setFacts(facts);
+        setFetchError(null);
+      }
+    }
+    fetchData();
+  },[facts])
 
 
   return (
